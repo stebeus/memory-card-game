4e6c6e2600d5bf0ae5adab57cf1b8a508b7979e6
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-28 12:55:30 -0300

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from '@/components/ui/Link.jsx';
+
+export function Header() {
+  return (
+    <header>
+      <h1 className="logo">
+        <Link>Memory card</Link>
+      </h1>
+    </header>
+  );
+}
