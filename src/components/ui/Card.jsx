34977d0a09ccbe3e9a29dc680a14b408f58a522f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-28 16:37:41 -0300

    feat: add Card component

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+export function Card({ id }) {
+  const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+  const [pokemon, setPokemon] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon-form/${id}`;
+
+      try {
+        const response = await fetch(pokemonUrl);
+        const data = await response.json();
+        setPokemon(data.name);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPokemon(id);
+  }, [id]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="card" data-pokemon={pokemon}>
+      <h2>{pokemon}</h2>
+      <img src={spriteUrl} alt={pokemon} width={160} height={160} />
+    </div>
+  );
+}
