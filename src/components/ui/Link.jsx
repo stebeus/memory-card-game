a73623efcee536b5f32928a82782fd5529647efe
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-29 11:01:15 -0300

    fix: correct isExternal scheme test

--- src/components/ui/Link.jsx
+++ src/components/ui/Link.jsx
@@ -2,7 +2,7 @@ import { BASE_URL } from '@/constants.js';
 
 export function Link({ to = BASE_URL, children }) {
   const urlScheme = /http|https/;
-  const isExternal = urlScheme.test(`${to}//:`);
+  const isExternal = urlScheme.test(`${to}://`);
 
   const isTargetBlank = isExternal && {
     target: '_blank',
