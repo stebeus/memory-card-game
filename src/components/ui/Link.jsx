6358f55eb2ee8d5b30dbdb7f107cfc2c4c444c98
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-29 14:32:27 -0300

    refactor: use string instead of regex

--- src/components/ui/Link.jsx
+++ src/components/ui/Link.jsx
@@ -1,8 +1,8 @@
 import { BASE_URL } from '@/constants.js';
 
 export function Link({ to = BASE_URL, children }) {
-  const urlScheme = /http:[//]|https:[//]/;
-  const isExternal = urlScheme.test(to);
+  const protocol = 'https://';
+  const isExternal = to.includes(protocol);
 
   const isTargetBlank = isExternal && {
     target: '_blank',
