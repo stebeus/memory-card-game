786d44b142c24bcdaa90c10465c52654f50cce80
stebeus <189894010+stebeus@users.noreply.github.com>
2026-03-29 14:56:00 -0300

    feat: add tap handler

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,30 @@ import './assets/App.css';
 export function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [tappedCards, setTappedCards] = useState([]);
+
+  function handleTap({ target }) {
+    const card = target.closest('[data-pokemon]');
+    const pokemon = card.dataset.pokemon;
+
+    if (tappedCards.includes(pokemon)) {
+      setBestScore(score > bestScore ? score : bestScore);
+      setScore(0);
+      setTappedCards([]);
+
+      return;
+    }
+
+    setScore(score + 1);
+    setTappedCards([...tappedCards, pokemon]);
+  }
 
   return (
     <>
       <Header score={score} bestScore={bestScore} />
       <main>
         <p>Tap a card to score, but never click the same Pokémon twice!</p>
-        <Grid />
+        <Grid cardHandler={handleTap} />
       </main>
       <Footer />
     </>
